33d1619a88ca971d23deb4f4e44303d2b0c499e6
callumok2004 <callumokane123@gmail.com>
2021-11-14 14:23:21 +0000

    Refactor

--- src/modules/News/events/messageCreate.ts
+++ src/modules/News/events/messageCreate.ts
@@ -5,7 +5,6 @@ export = {
 	type: "client",
 	async run(client: Bot, message: Message) {
 		if (!client.config.newsChannels.includes(message.channel.id) || message.content.length <= 0) return;
-		console.log(message.content.length > 0);
 
 		message.react("📰")
 		setTimeout(() => { message.reactions.resolve("📰")?.remove(); }, 10000);
--- src/modules/Radio/index.ts
+++ src/modules/Radio/index.ts
@@ -13,8 +13,6 @@ export const updateRadios = () => {
     const file = require(`${process.cwd()}/radios/services/${x.name}`),
       radio = await file();
 
-    console.log(radio.name)
-
     await collection.findOneAndUpdate(
       { id: radio.id },
       { $set: { ...radio, lowerName: radio.name.toLowerCase(), lowerAliases: radio.aliases?.map(x => x.toLowerCase()) || [] } },
